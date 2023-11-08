db9d8de61f41ef9f350627998046b6d803e36920
ZeddBao <baozeyubzy@gmail.com>
2023-11-08 22:58:39 +0800

    update

--- test.py
+++ test.py
@@ -1,12 +1,13 @@
 import torch
-from mlp import MLP
+from mlp import MLP, MLP2
 from tqdm import tqdm
 from torch.utils.data import DataLoader
 
 # model = MLP(63, 128, 10)
 # model = MLP(63, 64, 10)
-model = MLP(63, 32, 10)
+# model = MLP(63, 32, 10)
 # model = MLP(63, 16, 10)
+model = MLP2(63, 16, 10)
 
 # quantize
 # model.qconfig = torch.quantization.default_qconfig
@@ -14,16 +15,16 @@ model = MLP(63, 32, 10)
 # model = torch.quantization.convert(model, inplace=True)
 
 # 加载模型
-model.load_state_dict(torch.load('ckpt/1101_23/model.pth'))
+model.load_state_dict(torch.load('ckpt/1108_19/model.pth'))
 # 载入gpu
 device = torch.device('cuda:0')
 # device = torch.device('cpu')
 model = model.to(device)
 model.eval()
-model.half()
+# model.half()
 
 def test(dataset_test):
-    dataloader = DataLoader(dataset_test, batch_size=1, shuffle=False)
+    dataloader = DataLoader(dataset_test, batch_size=10000, shuffle=False)
 
     correct = 0
     total = 0
@@ -34,7 +35,8 @@ def test(dataset_test):
             labels = labels.to(device)
             # print('label.shape', labels.shape)
             # input = hand_landmarks.view(current_batch_size, -1).to(device)
-            input = hand_landmarks.view(current_batch_size, -1).to(device).half()
+            input = hand_landmarks.view(current_batch_size, -1).to(device)
+            # input = hand_landmarks.view(current_batch_size, -1).to(device).half()
             output = model(input)
             output = torch.softmax(output, dim=1)
             correct += (output.argmax(dim=1) == labels.argmax(dim=1)).sum().item()
