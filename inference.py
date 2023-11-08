f77d5c209eaf0b15435c220a3136ef6ff995a58b
zzz <baozeyubzy@gmail.com>
2023-11-08 18:58:03 +0800

    update

--- inference.py
+++ inference.py
@@ -7,9 +7,9 @@ from torch.utils.data import DataLoader
 
 # model = MLP(63, 128, 10)
 # model = MLP(63, 64, 10)
-model = MLP(63, 16, 10)
+model = MLP(63, 32, 10)
 # 加载模型
-model.load_state_dict(torch.load('ckpt/1102_00/model.pth'))
+model.load_state_dict(torch.load('ckpt/1101_23/model.pth'))
 # 载入gpu
 device = torch.device('cuda:0')
 model = model.to(device)
